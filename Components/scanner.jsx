37ed52e1ae09e56cb8cf2ff1f128d5d5596e7b41
Archit <architojha420@gmail.com>
2024-02-27 03:59:17 +0530

    added camera permission

--- Components/scanner.jsx
+++ Components/scanner.jsx
@@ -1,12 +1,74 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  PermissionsAndroid,
+  Dimensions,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
 
 const Scanner = () => {
+  const {height, width} = Dimensions.get('window');
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
+
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: 'Camera Permission',
+          message: 'App needs camera permission to take pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        setHasPermission(true);
+      } else {
+        setHasPermission(false);
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={{flex: 1, alignItems: 'center', marginTop: height * 0.2}}>
+        <Text style={{color: 'black'}}>No access to camera</Text>
+        <TouchableOpacity
+          onPress={() => setTimeout(() => requestCameraPermission(), 0)}>
+          <Text
+            style={{
+              color: 'black',
+              color: '#0c4ca3',
+              backgroundColor: '#d6e8fc',
+              padding: width * 0.04,
+              borderRadius: 20,
+              paddingHorizontal: 10,
+            }}>
+            Request Camera Permission
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
-    <View style={{flex:1, backgroundColor: 'red'}}>
+    <View style={{flex: 1, backgroundColor: 'red'}}>
       <Text>Scanner</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Scanner
+export default Scanner;
