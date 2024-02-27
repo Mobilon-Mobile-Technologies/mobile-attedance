9f8c4e8ce935e05184194bfa435ae9f2ed17dbcd
Archit <architojha420@gmail.com>
2024-02-27 20:47:42 +0530

    added token validity check

--- Components/scanner.jsx
+++ Components/scanner.jsx
@@ -6,18 +6,80 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import React, {useEffect, useState, useRef} from 'react';
-import {Camera, useCameraDevice,useCodeScanner} from 'react-native-vision-camera';
+import {
+  Camera,
+  useCameraDevice,
+  useCodeScanner,
+} from 'react-native-vision-camera';
 
 const Scanner = () => {
   const {height, width} = Dimensions.get('window');
   const [hasPermission, setHasPermission] = useState(null);
   const device = useCameraDevice('back');
+  const [scanned, setScanned] = useState(false);
+
   const codeScanner = useCodeScanner({
     codeTypes: ['qr', 'ean-13'],
-    onCodeScanned: (codes) => {
-      console.log(`Scanned ${codes.length} codes!`)
+    onCodeScanned: code => {
+      extractData(code);
+      setScanned(true);
+    },
+  });
+
+  const extractData = (code) => {
+    const data = code.map(item => {
+      const url=new URL(item.value);
+      const temp=getTokenFromUrl(url);
+      console.log(url);
+      checkTokenValidity(temp.token, temp.coursecode, temp.groupcode)
+
+    });
+  };
+
+  const getTokenFromUrl = (url) => {
+    const params = {};
+    const regex = /[?&]([^=#]+)=([^&#]*)/g;
+    let match;
+    while ((match = regex.exec(url)) !== null) {
+        params[match[1]] = decodeURIComponent(match[2]);
     }
-  })
+    return params;
+  };
+
+  const checkTokenValidity = async (token, coursecode, groupcode) => {
+    try {
+      const response = await fetch(
+        `https://sixc1f0487-145f-4e33-8897-641d33f1d0e6.onrender.com/check_status/${token}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data.status);
+
+        if (data.status === "valid") {
+          console.log('you scanned a valid qr');
+
+
+          // updateData(userdata, coursecode, groupcode, token);
+
+        } else {
+          console.log("you scanned a inalid qr code");
+          setScanned(false);
+        }
+      } else {
+        throw new Error("Network response was not ok.");
+      }
+    } catch (error) {
+      console.error("Error during token validity check:", error);
+    }
+  };
+  
 
   useEffect(() => {
     requestCameraPermission();
@@ -70,18 +132,25 @@ const Scanner = () => {
     );
   }
 
-
+  
 
   return (
     <View style={{flex: 1}}>
-      <View style={{justifyContent: 'center', alignItems: 'center', marginTop: height*0.1}}>
-        <Camera
-          style={{height: height * 0.5, width: width * 0.8}}
-          device={device}
-          isActive={true}
-          codeScanner={codeScanner}
-        />
-      </View>
+      {scanned ? <Text style={{color: 'black'}}>Scanned</Text> : (
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: height * 0.1,
+          }}>
+          <Camera
+            style={{height: height * 0.5, width: width * 0.8}}
+            device={device}
+            isActive={true}
+            codeScanner={codeScanner}
+          />
+        </View>
+      )}
     </View>
   );
 };
